dccfa7114a78889263c6efb4614f8ce28c8ffe8e
samuraii <7thsamurai@mail.ru>
2018-03-27 18:39:11 +0000

    Правки по ревью

--- bars.py
+++ bars.py
@@ -1,28 +1,14 @@
 import json
-import requests
-import os
+import sys
 import math
 
 
-def get_user_api_key():
+def get_data_from_file(path_to_file):
     try:
-        with open('api_key') as file_with_key:
-            api_key = file_with_key.read().strip()
-        return {'api_key': api_key}
+        with open(path_to_file) as file_data:
+            return json.loads(file_data.read())['features']
     except (FileNotFoundError, IOError):
-        print('Отсутствует файл api_key в корневой папке')
-
-
-def fetch_data_from_api(api_url, api_key):
-    try:
-        return requests.get(api_url, params=api_key)
-    except requests.exceptions.HTTPError:
-        print('Ошибка получения данных')
-
-
-def create_bar_data_file(data_to_write):
-    with open('bar_data.json', 'w+') as data_file:
-        data_file.write(data_to_write.text)
+        print('Файл не найден.')
 
 
 def get_bar_seats(bar):
@@ -31,15 +17,6 @@ def get_bar_seats(bar):
 
 def get_bar_name(bar):
     return bar['properties']['Attributes']['Name']
-
-
-def get_bar_address(bar):
-    return bar['properties']['Attributes']['Address']
-
-
-def load_bar_data():
-    with open('bar_data.json', 'r') as bars_data:
-        return json.loads(bars_data.read())['features']
 
 
 def get_biggest_bar(bars_data):
@@ -61,8 +38,8 @@ def get_user_coordinates():
 
 
 def get_bar_coordinates(bar_data):
-    bar_longtitude = float(bar_data['geometry']['coordinates'][0])
-    bar_latitude = float(bar_data['geometry']['coordinates'][1])
+    bar_longtitude = bar_data['geometry']['coordinates'][0]
+    bar_latitude = bar_data['geometry']['coordinates'][1]
     return bar_longtitude, bar_latitude
 
 
@@ -76,23 +53,17 @@ def calculate_distance(user_coordinates, bar_coordinates):
 def get_closest_bar(bars_data):
     user_coordinates = get_user_coordinates()
     return min(
-                bars_data, 
-                key=lambda bar_data: calculate_distance(
-                                user_coordinates, 
-                                get_bar_coordinates(bar_data)
-                )
+        bars_data, 
+        key=lambda bar_data: calculate_distance(
+            user_coordinates, 
+            get_bar_coordinates(bar_data)
+        )
     )
 
 
 if __name__ == '__main__':
     
-    if not os.path.isfile('bar_data.json'):
-        api_url = 'https://apidata.mos.ru/v1/features/1796'
-        api_key = get_user_api_key()
-        data_from_url = fetch_data_from_api(api_url, api_key)
-        create_bar_data_file(data_from_url)
-
-    bars_data = load_bar_data()
+    bars_data = get_data_from_file(sys.argv[1])
     biggest_bar = get_biggest_bar(bars_data)
     smallest_bar = get_smallest_bar(bars_data)
     closest_bar = get_closest_bar(bars_data)
