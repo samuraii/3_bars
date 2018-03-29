cf38b11eeafd16084370dd340c3cb87b06c207a6
campside <chirkovv.ma@yandex.ru>
2018-03-29 21:22:22 +0300

    Уменьшил сложность

--- bars.py
+++ bars.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
     except FileNotFoundError:
         exit('Не могу найти такой файл.')
 
-    try:
+    if bars_data:
         biggest_bar = get_biggest_bar(bars_data)
         smallest_bar = get_smallest_bar(bars_data)
         print('Самый большой бар: {}'.format(get_bar_name(biggest_bar)))
@@ -76,5 +76,5 @@ if __name__ == '__main__':
         user_coordinates = get_user_coordinates()
         closest_bar = get_closest_bar(bars_data, user_coordinates)
         print('Самый близкий бар: {}'.format(get_bar_name(closest_bar)))
-    except TypeError:
+    else:
         exit('Содержимое файла не валидно, проверь что там.')
